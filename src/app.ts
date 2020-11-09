f0c83cf4e0c11d47562e0ef2bafeb81c5f16f5ea
Marc Dos Santos <mdossantosdev@gmail.com>
2020-11-09 13:21:57 +0100

    Install morgan dependencie and add to app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import logger from 'morgan';
 import routes from './routes';
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(logger('dev'));
+}
 
 app.use('/api/v1', routes);
 
