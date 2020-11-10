6b3eb82c60c61c8a41a18d5403f821c5ea9a8592
Marc Dos Santos <mdossantosdev@gmail.com>
2020-11-10 11:13:02 +0100

    Update createMerchant from adminController

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { Merchant } from '../models/Merchant';
 
 export const createMerchant = async (req: Request, res: Response) => {
   const {
@@ -12,15 +13,27 @@ export const createMerchant = async (req: Request, res: Response) => {
   } = req.body;
 
   try {
-    res.json({
+    const existingMerchant = await Merchant.findOne({ email });
+
+    if (existingMerchant) {
+      return res.json({ message: 'Merchant already exists'})
+    };
+
+    const newMerchant = await Merchant.create({
       name,
       foodType,
       address,
       phone,
       email,
       password,
-      owner
+      owner,
+      serviceAvailable: false,
+      coverImages: [],
+      rating: 0
     });
+
+
+    res.json(newMerchant);
   } catch (error) {
     console.error(error);
     res.status(500).send('Server Error');
