596fd7eec4aae5ff83e00db8a5b8b0b8436c8a45
Marc Dos Santos <mdossantosdev@gmail.com>
2020-11-10 13:03:22 +0100

    Install dotenv and update config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
-const DB_CLUSTER = '';
-const DB_NAME = '';
-const DB_USERNAME = '';
-const DB_PASSWORD = '';
+const DB_CLUSTER = process.env.DB_CLUSTER;
+const DB_NAME = process.env.DB_NAME;
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
 
 export const DB = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_CLUSTER}/${DB_NAME}?retryWrites=true&w=majority`;
