f10056949ad6380919ab2534c59a061d89a0e838
Marc Dos Santos <mdossantosdev@gmail.com>
2020-12-01 11:08:51 +0100

    Add getTopRestaurants controller and route

--- src/controllers/shopController.ts
+++ src/controllers/shopController.ts
@@ -19,3 +19,22 @@ export const getFoodAvailability = async (req: Request, res: Response) => {
     console.log(error);
   }
 };
+
+export const getTopRestaurants = async (req: Request, res: Response) => {
+  const zipCode = req.params.zipCode;
+
+  try {
+    const restaurants = await Merchant
+      .find({ zipCode, serviceAvailable: true })
+      .sort([['rating', 'descending']])
+      .limit(10);
+
+      if (restaurants.length === 0) {
+        return res.status(404).json({ message: 'Data not found!' });
+      }
+
+      return res.status(200).json(restaurants);
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/routes/shopRoutes.ts
+++ src/routes/shopRoutes.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response } from 'express';
-import { getFoodAvailability } from '../controllers/shopController';
+import {
+  getFoodAvailability,
+  getTopRestaurants
+} from '../controllers/shopController';
 
 const router = express.Router();
 
 router.get('/:zipCode', getFoodAvailability);
+router.get('/top-restaurant/:zipCode', getTopRestaurants);
 
 router.get('/', (req: Request, res: Response) => {
   return res.json({ message: 'Hello from Shop' });
