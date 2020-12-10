ac94ba4225e016aac99fbe56433aa6563409fb0d
Marc Dos Santos <mdossantosdev@gmail.com>
2020-12-10 10:24:13 +0100

    Init customer route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import adminRouter from './adminRoutes';
 import merchantRouter from './merchantRoutes';
 import shopRouter from './shopRoutes';
+import customerRouter from './customerRoutes';
 
 const router = express.Router();
 
 router.use('/admin', adminRouter);
 router.use('/merchant', merchantRouter);
 router.use('/shop', shopRouter);
+router.use('/customer', customerRouter);
 
 export default router;
