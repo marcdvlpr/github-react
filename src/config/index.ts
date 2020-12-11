72ddd8f0f5d1eeed3a5c65c8cba070c06f65a992
Marc Dos Santos <mdossantosdev@gmail.com>
2020-12-11 13:56:03 +0100

    Add requestOtp function

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,3 +6,7 @@ const DB_PASSWORD = process.env.DB_PASSWORD;
 export const DB = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_CLUSTER}/${DB_NAME}?retryWrites=true&w=majority`;
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const TOKEN_LIFE = process.env.TOKEN_LIFE;
+
+export const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
+export const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
+export const TWILIO_PHONE_NUMBER = process.env.TWILIO_PHONE_NUMBER;
--- src/helpers/auth.ts
+++ src/helpers/auth.ts
@@ -1,8 +1,15 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import jwt, { SignOptions } from 'jsonwebtoken';
+import { Twilio } from 'twilio';
 import { AuthPayload } from '../interfaces/IAuth';
-import { JWT_SECRET, TOKEN_LIFE } from '../config';
+import {
+  JWT_SECRET,
+  TOKEN_LIFE,
+  TWILIO_ACCOUNT_SID,
+  TWILIO_AUTH_TOKEN,
+  TWILIO_PHONE_NUMBER
+} from '../config';
 
 export const generatePasswordHash = async (password: string) => {
   const salt = await bcrypt.genSalt(10);
@@ -42,4 +49,25 @@ export const generateOtp = () => {
   otpExpiry.setTime(new Date().getTime() + (30 * 60 * 1000));
 
   return { otp, otpExpiry };
+};
+
+export const requestOtp = async (otp: number, phoneNumber: string) => {
+  try {
+    const accountSid = TWILIO_ACCOUNT_SID;
+    const authToken = TWILIO_AUTH_TOKEN;
+
+    if (accountSid && authToken) {
+      const client = new Twilio(accountSid, authToken);
+
+      const response = await client.messages.create({
+        from: TWILIO_PHONE_NUMBER,
+        to: phoneNumber,
+        body: `Your OTP verification code is: ${otp}`
+      });
+
+      return response;
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
