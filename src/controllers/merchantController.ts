8303a12dca1b64b78a009a657e18208df566ef54
Marc Dos Santos <mdossantosdev@gmail.com>
2021-01-02 14:52:13 +0100

    Refactor merchant controllers

--- src/controllers/merchantController.ts
+++ src/controllers/merchantController.ts
@@ -1,14 +1,12 @@
 import { Request, Response } from 'express';
-import { Merchant } from '../models/Merchant';
-import { Food } from '../models';
+import { Merchant, Food } from '../models';
 import { validatePassword, generateToken } from '../helpers/auth';
 import { IMerchantLoginInput, IEditMerchantInput } from '../interfaces/IMerchant';
 import { ICreateFoodItemInput } from '../interfaces/IFood';
 
 export const merchantLogin = async (req: Request, res: Response) => {
-  const { email, password } = <IMerchantLoginInput>req.body;
-
   try {
+    const { email, password } = <IMerchantLoginInput>req.body;
     const merchant = await Merchant.findOne({ email }).select('+password');
 
     if (!merchant) {
@@ -29,122 +27,134 @@ export const merchantLogin = async (req: Request, res: Response) => {
 
     const token = generateToken(payload);
 
-    res.json({ token });
+    return res.status(200).json({ token });
   } catch (error) {
-    console.error(error);
+    if (error instanceof Error) console.error(error.message);
+    return res.status(500).send('Server Error');
   }
 };
 
 export const getMerchantProfile = async (req: Request, res: Response) => {
   try {
-    const merchant = await Merchant.findById(req.user?._id);
+    const user = req.user;
+    const merchant = await Merchant.findById(user?._id);
 
-    return res.json(merchant);
+    return res.status(200).json(merchant);
   } catch (error) {
-    console.error(error);
+    if (error instanceof Error) console.error(error.message);
+    return res.status(500).send('Server Error');
   }
 };
 
 export const updateMerchantProfile = async (req: Request, res: Response) => {
-  const { name, foodType, address, phone } = <IEditMerchantInput>req.body;
-
-  const merchantField = {} as IEditMerchantInput;
-
-  if (name) merchantField.name = name;
-  if (foodType) merchantField.foodType = foodType;
-  if (address) merchantField.address = address;
-  if (phone) merchantField.phone = phone;
-
   try {
+    const user = req.user;
+    const { name, foodType, address, phone } = <IEditMerchantInput>req.body;
+
+    const merchantField = {} as IEditMerchantInput;
+
+    if (name) merchantField.name = name;
+    if (foodType) merchantField.foodType = foodType;
+    if (address) merchantField.address = address;
+    if (phone) merchantField.phone = phone;
+
     const updateMerchant = await Merchant.findByIdAndUpdate(
-      req.user?._id,
+      user?._id,
       { $set: merchantField },
       { new: true, upsert: true }
     );
 
     return res.status(200).json(updateMerchant);
   } catch (error) {
-    console.error(error);
+    if (error instanceof Error) console.error(error.message);
+    return res.status(500).send('Server Error');
   }
 };
 
 export const updateMerchantCoverImage = async (req: Request, res: Response) => {
   try {
-    const merchant = await Merchant.findById(req.user?._id);
+    const user = req.user;
+    const merchant = await Merchant.findById(user?._id);
 
-    if (merchant !== null) {
-      const files = req.files as Express.Multer.File[];
-      const images = files.map((file: Express.Multer.File) => file.filename);
-
-      merchant.coverImages.push(...images);
-
-      const saveResult = await merchant.save();
-
-      return res.status(200).json(saveResult);
+    if (!merchant) {
+      return res.status(400).json({ message: 'Unable to update merchant profile!' });
     }
 
-    return res.status(400).json({ message: 'Unable to update merchant profile' });
+    const files = req.files as Express.Multer.File[];
+    const images = files.map((file: Express.Multer.File) => file.filename);
+
+    merchant.coverImages.push(...images);
+
+    const saveResult = await merchant.save();
+
+    return res.status(200).json(saveResult);
   } catch (error) {
-    console.error(error);
+    if (error instanceof Error) console.error(error.message);
+    return res.status(500).send('Server Error');
   }
 };
 
 export const updateMerchantService = async (req: Request, res: Response) => {
   try {
-    const merchant = await Merchant.findById(req.user?._id);
+    const user = req.user;
+    const merchant = await Merchant.findById(user?._id);
 
-    if (merchant !== null) {
-      merchant.serviceAvailable = !merchant.serviceAvailable;
-
-      const saveResult = await merchant.save();
-      return res.status(200).json(saveResult);
+    if (!merchant) {
+      return res.status(400).json({ message: 'Unable to update merchant profile!' });
     }
 
+    merchant.serviceAvailable = !merchant.serviceAvailable;
 
-    return res.status(400).json({ message: 'Unable to update merchant profile' })
+    const saveResult = await merchant.save();
+
+    return res.status(200).json(saveResult);
   } catch (error) {
-    console.error(error);
+    if (error instanceof Error) console.error(error.message);
+    return res.status(500).send('Server Error');
   }
 };
 
 export const addFoodItem = async (req: Request, res: Response) => {
-  const { name, description, category, foodType, readyTime, price } = <ICreateFoodItemInput>req.body;
-
   try {
-    const merchant = await Merchant.findById(req.user?._id);
+    const user = req.user;
+    const { name, description, category, foodType, readyTime, price } = <ICreateFoodItemInput>req.body;
 
-    if (merchant !== null) {
-      const files = req.files as [Express.Multer.File];
-      const images = files.map((file: Express.Multer.File) => file.filename);
+    const merchant = await Merchant.findById(user?._id);
 
-      const foodItem = await Food.create({
-        merchantId: merchant._id,
-        name,
-        description,
-        category,
-        foodType,
-        readyTime,
-        price,
-        rating: 0,
-        images
-      })
-
-      merchant.foods.push(foodItem);
-
-      const merchantProfile = await merchant.save();
-
-      return res.status(201).json(merchantProfile);
+    if (!merchant) {
+      return res.status(400).json({ message: 'Unable to update merchant profile!'});
     }
 
-    return res.status(400).json({ message: 'Unable to update merchant profile'});
+    const files = req.files as [Express.Multer.File];
+    const images = files.map((file: Express.Multer.File) => file.filename);
+
+    const foodItem = await Food.create({
+      merchantId: merchant._id,
+      name,
+      description,
+      category,
+      foodType,
+      readyTime,
+      price,
+      rating: 0,
+      images
+    })
+
+    merchant.foods.push(foodItem);
+
+    const merchantProfile = await merchant.save();
+
+    return res.status(201).json(merchantProfile);
   } catch (error) {
-    console.error(error);
+    if (error instanceof Error) console.error(error.message);
+    return res.status(500).send('Server Error');
   }
 };
 
 export const getFoods = async (req: Request, res: Response) => {
   try {
-    const foods = await Food.find({ merchantId: req.user?._id });
+    const user = req.user;
+    const foods = await Food.find({ merchantId: user?._id });
 
     if (foods.length === 0) {
       return res.status(404).json({ message: 'Foods not found!' });
@@ -152,6 +162,7 @@ export const getFoods = async (req: Request, res: Response) => {
 
     return res.status(200).json(foods);
   } catch (error) {
-    console.error(error);
+    if (error instanceof Error) console.error(error.message);
+    return res.status(500).send('Server Error');
   }
 };
