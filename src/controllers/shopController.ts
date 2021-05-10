453a4c5b3c490f238ec0b23ccdb0b5bd205eeb19
Marc Dos Santos <mdossantosdev@gmail.com>
2021-05-10 13:58:02 +0200

    Refactor shop controllers

--- src/controllers/shopController.ts
+++ src/controllers/shopController.ts
@@ -4,15 +4,12 @@ import { Merchant, IFoodModel, Offer } from '../models';
 export const getFoodAvailability = async (req: Request, res: Response) => {
   try {
     const zipCode = req.params.zipCode;
-
     const restaurants = await Merchant
       .find({ zipCode, serviceAvailable: true })
       .sort([['rating', 'descending']])
       .populate('foods');
 
-    if (restaurants.length === 0) {
-      return res.status(404).json({ message: 'Data not found!' });
-    }
+    if (restaurants.length === 0) return res.status(404).json({ message: 'Data not found!' });
 
     return res.status(200).json(restaurants);
   } catch (error) {
@@ -24,17 +21,14 @@ export const getFoodAvailability = async (req: Request, res: Response) => {
 export const getTopRestaurants = async (req: Request, res: Response) => {
   try {
     const zipCode = req.params.zipCode;
-
     const restaurants = await Merchant
       .find({ zipCode, serviceAvailable: true })
       .sort([['rating', 'descending']])
       .limit(10);
 
-      if (restaurants.length === 0) {
-        return res.status(404).json({ message: 'Data not found!' });
-      }
+    if (restaurants.length === 0) return res.status(404).json({ message: 'Data not found!' });
 
-      return res.status(200).json(restaurants);
+    return res.status(200).json(restaurants);
   } catch (error) {
     if (error instanceof Error) console.error(error.message);
     return res.status(500).send('Server Error');
@@ -44,15 +38,12 @@ export const getTopRestaurants = async (req: Request, res: Response) => {
 export const getFoodsIn30Min = async (req: Request, res: Response) => {
   try {
     const zipCode = req.params.zipCode;
-
     const restaurants = await Merchant
       .find({ zipCode, serviceAvailable: true })
       .sort([['rating', 'descending']])
       .populate('foods');
 
-    if (restaurants.length === 0) {
-      return res.status(404).json({ message: 'Data not found!' });
-    }
+    if (restaurants.length === 0) return res.status(404).json({ message: 'Data not found!' });
 
     let foodResult: any = [];
 
@@ -72,14 +63,9 @@ export const getFoodsIn30Min = async (req: Request, res: Response) => {
 export const searchFoods = async (req: Request, res: Response) => {
   try {
     const zipCode = req.params.zipCode;
+    const restaurants = await Merchant.find({ zipCode, serviceAvailable: true }).populate('foods');
 
-    const restaurants = await Merchant
-      .find({ zipCode, serviceAvailable: true })
-      .populate('foods');
-
-    if (restaurants.length === 0) {
-      return res.status(404).json({ message: 'Data not found!' });
-    }
+    if (restaurants.length === 0) return res.status(404).json({ message: 'Data not found!' });
 
     let foodResult: any = [];
 
@@ -97,9 +83,7 @@ export const getRestaurantById = async (req: Request, res: Response) => {
     const id = req.params.id;
     const restaurant = await Merchant.findById(id).populate('foods');
 
-    if (!restaurant) {
-      return res.status(404).json({ message: 'Data not found!' });
-    }
+    if (!restaurant) return res.status(404).json({ message: 'Data not found!' });
 
     return res.status(200).json(restaurant);
   } catch (error) {
@@ -111,12 +95,9 @@ export const getRestaurantById = async (req: Request, res: Response) => {
 export const getOffers = async (req: Request, res: Response) => {
   try {
     const zipCode = req.params.zipCode;
-
     const offers = await Offer.find({ zipCode, isActive: true });
 
-    if (!offers) {
-      return res.status(404).json({ message: 'Offers not found!' });
-    }
+    if (!offers) return res.status(404).json({ message: 'Offers not found!' });
 
     return res.status(200).json(offers);
   } catch (error) {
